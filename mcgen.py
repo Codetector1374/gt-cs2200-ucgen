f2009453d9d4650800b323c1caae475ea73acbef
codetector <codetector@codetector.cn>
2019-09-15 15:19:41 -0400

    Mostly works (Need CmpROM)

--- mcgen.py
+++ mcgen.py
@@ -1,39 +1,63 @@
 
-import argparse
-import re
 from typing import List
+import argparse
+import math
+import heapq
+from antlr4 import *
+from uclang.uclangLexer import uclangLexer
+from uclang.uclangParser import uclangParser
 
 AvailableSignals = {
-    'NextState': 6,
-    'DrReg': 1,
-    'DrMEM': 1,
-    'DrALU': 1,
-    'DrPC': 1,
-    'DrOFF': 1,
-    'LdPC': 1,
-    'LdIR': 1,
-    'LdMAR': 1,
-    'LdA': 1,
-    'LdB': 1,
-    'LdCmp': 1,
-    'WrReg': 1,
-    'WrMEM': 1,
-    'RegSel': 2,
-    'ALU': 2,
-    'OPTest': 1,
-    'ChkCmp': 1,
+    'nextstate': 6,
+    'drreg': 1,
+    'drmem': 1,
+    'dralu': 1,
+    'drpc': 1,
+    'droff': 1,
+    'ldpc': 1,
+    'ldir': 1,
+    'ldmar': 1,
+    'lda': 1,
+    'ldb': 1,
+    'ldcmp': 1,
+    'wrreg': 1,
+    'wrmem': 1,
+    'regsel': 2,
+    'alu': 2,
+    'optest': 1,
+    'chkcmp': 1,
 }
 
 MainRomSignalValues = {}
-
 MainRomSymbolTable = {}
-ValueTable = []
+MainRomEntries = []
+MainRomContent = []
+SequencerRomContent = []
+SequencerRomEntries = []
+heapq.heapify(SequencerRomEntries)
 
-
-class MicrocodeSignal:
-    def __init__(self, parseValue):
+class SequencerEntry:
+    def __init__(self, name: str, value: int, target: str):
+        self.name = name
+        self.value = value
+        self.target = target
         pass
 
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self):
+        return "SEQ: (%s)[%01X]: %s" % (self.name, self.value, self.target)
+
+class MicrocodeSignal:
+    def __init__(self, name, value):
+        self.name:str = name
+        self.value:int = value
+        pass
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return '{%s}->%i' % (self.name, self.value)
 
 
 class MicrocodeEntry:
@@ -42,6 +66,12 @@ class MicrocodeEntry:
         self.signals = signals
         self.target = target
         pass
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return '\n%s: %s ==> %s' % (self.symbol, self.signals, self.target)
 
 
 def computeSignalValueTable():
@@ -53,55 +83,178 @@ def computeSignalValueTable():
     pass
 
 
-def parseLine(line, location):
-    line = line.replace('\r\n', '').replace('\n', '')
-    if len(line) == 0:
-        return None
+def toText(it: TerminalNode):
+    return it.getText()
 
-    matcher = re.compile(r'^(?P<symbol>[^:\[\] ]+)[ ]*:[ ]*'  # Line Starter Symbol
-                         r'(\[(?P<signals>[A-Za-z0-9=, ]*)\])[ ]*'  # Signal Assertion
-                         r'(?P<targetSymbol>[^:\[\] ]*)$')  # Redirection Symbol
-    result = matcher.search(line)
-    print(result.groupdict())
+class uclangListener(ParseTreeListener):
 
-    symbol = result.group('symbol').replace(' ', '')
-    if len(symbol) == 0:
-        print("ERROR: Line {} no symbol detected".format(location + 1))
+    def __init__(self):
+        self.location = 0
 
-    signals = result.group('signals').replace(' ', '').split(',')
-    print(signals)
+    # Exit a parse tree produced by uclangParser#ucSeqencerStmt.
+    def exitUcSequencerStmt(self, ctx:uclangParser.UcSequencerStmtContext):
+        ids = list(map(toText, ctx.IDENTIFIER()))
+        num = ctx.number().numVal
+        seqEnt = SequencerEntry(ids[0], num, ids[1])
+        heapq.heappush(SequencerRomEntries, (num, seqEnt))
+        pass
 
-    MainRomSymbolTable[symbol] = location
+    # Exit a parse tree produced by uclangParser#ucCmpStmt.
+    def exitUcCmpStmt(self, ctx:uclangParser.UcCmpStmtContext):
+        pass
 
+    # Exit a parse tree produced by uclangParser#ucSignalStmt.
+    def exitUcSignalStmt(self, ctx:uclangParser.UcSignalStmtContext):
+        ids = list(map(toText, ctx.IDENTIFIER()))
+        signal_list = ctx.signalList().signals().signalObjs
+        if len(ids) == 1:
+            stmt = MicrocodeEntry(ids[0], signal_list)
+        else:
+            stmt = MicrocodeEntry(ids[0], signal_list, ids[1])
+        ctx.stmt = stmt
+        MainRomSymbolTable[ids[0]] = self.location
+        self.location += 1
+        MainRomEntries.append(stmt)
+        pass
+
+    # Exit a parse tree produced by uclangParser#signalList.
+    def exitSignalList(self, ctx:uclangParser.SignalListContext):
+        ctx.signalObjs = ctx.signals().signalObjs
+        pass
+
+    # Exit a parse tree produced by uclangParser#signals.
+    def exitSignals(self, ctx:uclangParser.SignalsContext):
+        signals = ctx.signal()
+        signalObjs = []
+        for signal in signals:
+            if type(signal) == uclangParser.SignalContext:
+                signalObjs.append(signal.signalObj)
+        ctx.signalObjs = signalObjs
+        pass
+
+    # Exit a parse tree produced by uclangParser#signal.
+    def exitSignal(self, ctx:uclangParser.SignalContext):
+        signalName = ctx.IDENTIFIER().getText()
+
+        value = ctx.number()
+        if type(value) == uclangParser.NumberContext:
+           num_value = value.numVal
+        else:
+            num_value = 1
+        ctx.signalObj = MicrocodeSignal(signalName, num_value)
+        pass
+
+    # Exit a parse tree produced by uclangParser#number.
+    def exitNumber(self, ctx:uclangParser.NumberContext):
+        ctx.numVal = ctx.children[0].numVal
+        pass
+
+    # Exit a parse tree produced by uclangParser#dec_num.
+    def exitDec_num(self, ctx:uclangParser.Dec_numContext):
+        ctx.numVal = int(ctx.getText())
+
+    # Exit a parse tree produced by uclangParser#hex_num.
+    def exitHex_num(self, ctx:uclangParser.Hex_numContext):
+        ctx.numVal = eval(ctx.getText())
+
+    # Exit a parse tree produced by uclangParser#bin_num.
+    def exitBin_num(self, ctx:uclangParser.Bin_numContext):
+        ctx.numVal = eval(ctx.getText())
+
+
+def validate_signal(signal: MicrocodeSignal) -> bool:
+    lowerSignalName = signal.name.lower()
+    if lowerSignalName not in AvailableSignals:
+        print("\n\nERROR: SIG: %s is not a valid signal" % (signal.name))
+        return False
+    signalBits = AvailableSignals[lowerSignalName]
+    if signal.value >= (1 << signalBits):
+        print('\n\nERROR: Value for signal %s (%d) exceeds the max value (%d)' % (signal.name, signal.value, (1 << signalBits) - 1))
+        return False
+    return True
+
+def process_main_rom_entry(entry: MicrocodeEntry):
+    signalList = entry.signals
+    entryValue = 0
+    if entry.target is not None:
+        if entry.target not in MainRomSymbolTable:
+            print('Unrecognized symbol reference %s' % (entry.target))
+            exit(-1)
+        signalList.append(
+            MicrocodeSignal('nextstate', MainRomSymbolTable[entry.target])
+        )
+    for signal in signalList:
+        if not validate_signal(signal):
+            exit(-1)
+        entryValue += signal.value << MainRomSignalValues[signal.name.lower()]
+    MainRomContent.append("%07x" % entryValue)
+
+
+def resolve_sequencer_entry(entry: SequencerEntry) -> str:
+    if entry.target.lower() not in MainRomSymbolTable:
+        print('Symbol (%s) referenced in Sequencer (%s) is not found in main rom'
+              % (entry.target, entry.name))
+        exit(-1)
+    return '%07x' % MainRomSymbolTable[entry.target.lower()]
     pass
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Python uCGen')
-    parser.add_argument("-o", required=True, type=str)
+    parser.add_argument("-o", required=True, type=str, metavar='output_prefix')
+    parser.add_argument('file', type=str)
 
     args = parser.parse_args()
 
     print('Py_uCGen 9000')
     print(args)
+    # if args.file
 
     print('Counting Configured Main Rom Bits...')
     computeSignalValueTable()
     totalBits = 0
     for signal in AvailableSignals:
         totalBits += AvailableSignals[signal]
-    print('Main ROM is {} bits'.format(totalBits))
+    outputBits = math.ceil(math.log(1 << totalBits, 16))
+    print('Main ROM is {} bits, ({} digits of Hex)'.format(totalBits, outputBits))
 
-    filename = 'demomc.uc'
+    print('==================================')
 
-    print('\n\nParsing file: {}\n'.format(filename))
+    filename = args.file
+    input = FileStream(filename)
+    lexer = uclangLexer(input)
+    stream = CommonTokenStream(lexer)
+    parser = uclangParser(stream)
+    tree = parser.ucprog()  # Parse Root
 
-    with open(filename, 'r') as file:
-        location = 0
-        line = file.readline()
-        while line:
-            parseLine(line, location)
-            line = file.readline()
-            location += 1
+    listener = uclangListener()
+    walker = ParseTreeWalker()
+
+    walker.walk(listener, tree)
+
+    print('MAIN SYMBOL TABLE: ', MainRomSymbolTable)
+    print('MAIN Entries: ', MainRomEntries)
+    for entry in MainRomEntries:
+        process_main_rom_entry(entry)
+
+    while len(SequencerRomEntries) > 0:
+        entry = heapq.heappop(SequencerRomEntries)
+        resolvedValue = resolve_sequencer_entry(entry[1])
+        while len(SequencerRomContent) < entry[0]:
+            SequencerRomContent.append('%07X' % 0)
+        if len(SequencerRomContent) != entry[0]:
+            print("Something very bad happened")
+            exit(-1)
+        else:
+            SequencerRomContent.append(resolvedValue)
+        print(entry)
         pass
-    print(MainRomSymbolTable)
+    print('===========OUTPUT===============')
+    print("MAIN: ", MainRomContent)
+    print("SEQ:  ", SequencerRomContent)
+    output = args.o
+    with open(('%s_main.dat' % output), 'w') as mainFile:
+        mainFile.write(' '.join(MainRomContent))
+        mainFile.flush()
+    with open(('%s_seq.dat' % output), 'w') as seqFile:
+        seqFile.write(' '.join(SequencerRomContent))
+        seqFile.flush()
--- uclang.g4
+++ uclang.g4
@@ -2,18 +2,24 @@ grammar uclang;
 
 ucprog: ucStmt (NEWLINE ucStmt)*;
 
-ucStmt: (COMMENT|ucSignalStmt);
+ucStmt: (ucSignalStmt|ucSequencerStmt|ucCmpStmt|);
 ucSignalStmt: IDENTIFIER ':' signalList IDENTIFIER?;
+ucSequencerStmt: '@' IDENTIFIER ':' number IDENTIFIER;
+ucCmpStmt: '!' IDENTIFIER ':' number IDENTIFIER;
 
 signalList: '[' signals ']';
 signals: signal (',' signal)*;
-signal: IDENTIFIER ('=' NUMBER)?;
+signal: IDENTIFIER ('=' number)?;
 
-COMMENT: '//'~[\r\n]*;
+number: dec_num|hex_num|bin_num;
+dec_num: T_DEC;
+hex_num: T_HEX;
+bin_num: T_BIN;
+
+COMMENT: '//'~[\r\n]* -> skip;
 IDENTIFIER: [a-zA-Z_][0-9a-zA-Z_-]*;
-NUMBER: DEC | HEX | BIN;
-DEC: [1-9][0-9]*;
-HEX: '0x'[0-9A-Fa-f]+;
-BIN: '0b'[0-1]+;
+T_DEC: [0-9]+;
+T_HEX: '0x'[0-9A-Fa-f]+;
+T_BIN: '0b'[0-1]+;
 WHITESPACE: [ \t] -> skip;
 NEWLINE: [\r\n];
